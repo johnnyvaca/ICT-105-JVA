60d5ffc7f032f3516d2da32c0d0d2c07e1869641
johnnyvaca <Johnny.VACA-JARAMILLO@cpnv.ch>
2019-11-06 16:50:03 +0100

    ajout left null

--- Tennis/tennisStatements.sql
+++ Tennis/tennisStatements.sql
@@ -3,14 +3,24 @@
 /* Version : 28-SEP-2019*/
 
 -- 1ère requête
-SELECT  players.firstName, players.lastName, sponsors.name AS sponsorName FROM players
+SELECT  players.id, players.firstName, players.lastName, sponsors.name AS sponsorName FROM players
 inner join sponsors on players.sponsor_id = sponsors.id;
 
 -- 2ème requête
-SELECT players.firstName, players.lastName, sponsors.name AS sponsorName FROM players
+SELECT players.id AS player_id, sponsors.id AS sponsors_id, players.firstName, players.lastName, sponsors.name AS sponsorName FROM players
 left join sponsors on players.sponsor_id = sponsors.id;
  
 -- 3ème requête  
-SELECT players.firstName, players.lastName, sponsors.name AS sponsorName FROM players
+SELECT players.id AS player_id, sponsors.id AS sponsors_id, players.firstName, players.lastName, sponsors.name AS sponsorName FROM players
 right join sponsors on players.sponsor_id = sponsors.id;
+
+-- 4ème requête  
 SELECT * FROM players;
+
+-- 5ème requête  
+SELECT * FROM sponsors;
+
+-- 6ème requête  
+SELECT players.firstName, players.lastName, sponsors.name AS sponsorName FROM players
+left JOIN sponsors ON players.sponsor_id = sponsors.id
+WHERE players.sponsor_id IS NULL;
